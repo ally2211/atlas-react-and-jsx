a1402dd88abc6a3cb51d6857802cf788e4e7b480
ally2211 <amendieta.va@gmail.com>
2025-05-08 14:37:42 -0500

    render the app using root.render

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
 import "./index.css";
 
 // Render the App component to the root element
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// Render the App component to the root element
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
+root.render(<App />)
