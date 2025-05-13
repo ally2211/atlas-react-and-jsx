9249cb56921059ac1c2e8f08507b4bf4fa3801d5
ally2211 <amendieta.va@gmail.com>
2025-05-13 01:49:14 -0500

    more sections

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,15 @@ function App() {
           efficiently using a virtual DOM.
         </p>
       </Section>
+      <Section title="Benefits of react">
+      <ul>
+        <li>Reusable components for efficient development</li>
+        <li>Virtual DOM improves performance</li>
+        <li>Strong community and ecosystem</li>
+        <li>Seamless integration with other libraries</li>
+        <li>Great for building dynamic single-page applications (SPAs)</li>
+      </ul>
+      </Section>
     </div>
     </div>
 
