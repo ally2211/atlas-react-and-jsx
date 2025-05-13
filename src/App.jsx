b9d5dec19be8a24ede9964dd40345a41644856ea
ally2211 <amendieta.va@gmail.com>
2025-05-13 01:41:47 -0500

    section

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,20 @@ import Header from "./components/Header.jsx";
 function App() {
   return (
     <div>
+    <div>
       <Header />
     </div>
+     <div id="section">
+      <Section title="What is react?">
+        <p>
+          React is a JavaScript library for building user interfaces. It allows
+          developers to create reusable UI components and manage dynamic data
+          efficiently using a virtual DOM.
+        </p>
+      </Section>
+    </div>
+    </div>
+
   );
 }
 export default App;
